db2e26c24fe061c91de41a5e6aa3da7eeb9abab1
boggil3665 <boggil3665@daum.net>
2019-07-17 05:53:02 +0900

    [ADD] SignUp 선 하나 더 긋기

--- MoodChange/src/components/MyInfo/MyInfo.js
+++ MoodChange/src/components/MyInfo/MyInfo.js
@@ -7,17 +7,18 @@ export default class MyInfo extends Component {
       <div className="MyInfo">
         <div children="MyInfo-NavBar">
           <div className="MyInfo-NavBar-Profile">
-
+            프로필
+            <img className="MyInfo-NavBar-Profile-img" src="../../image/profileIcon.png"/>
           </div>
           <div className="MyInfo-NavBar-Account">
-          
+            내 계정
           </div>
           <div className="MyInfo-NavBar-MyBoard">
-          
+            내 게시물
           </div>
         </div>
         <div className="MyInfo-Main">
-        
+          메인
         </div>
       </div>
     )
