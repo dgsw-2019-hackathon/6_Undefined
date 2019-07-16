18fe7c864f7ad34fbf4eb7890d814649d89e5231
ttingho <myeongho020812@gmail.com>
2019-07-16 22:49:21 +0900

    [ADD] dlkjfjkd;

--- MoodChange/src/components/Home/Home.js
+++ MoodChange/src/components/Home/Home.js
@@ -1,10 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './Home.scss';
+import Logo from '../../image/logo.png';
+import News from '../../image/White.png';
+import profile from '../../image/1.jpg';
+import writeImg from '../../image/내 게시물white.png';
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        홈페이지 입니다.
+      <div className="Home">
+        <div className="Home-Navi">
+          <ul>
+            <li className="logo"><img src={Logo} alt="Logo" /></li>
+            <li className="news"><img src={News} alt="News" className="newsImg" />뉴스피드</li>
+            <li className="profile"><img src={profile} alt="profile" />안채원</li>
+          </ul>
+
+          <button><img src={writeImg} alt="writeImg" />게시물 작성</button>
+        </div>
       </div>
     )
   }
--- MoodChange/src/components/LoginSignUp/Login/Login.js
+++ MoodChange/src/components/LoginSignUp/Login/Login.js
@@ -1,7 +1,51 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { SERVER } from '../../../config/config.json';
+import Swal from 'sweetalert2';
+// import sha512 from 'js-sha512';
 import './Login.scss';
 
 class Login extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        id: '',
+        pw: ''
+      };
+      console.log(props);
+    }
+
+    handleChange = e => {
+      this.setState({
+        [e.target.name]: e.target.value
+      });
+    };
+
+    handleLogin = async _ => {
+        await axios
+          .post(`${SERVER}/api/login/login_process`, {
+            user_id: this.state.id,
+            user_password: this.state.pw
+          })
+          .then(async e => {
+            await Swal.fire({
+                type: 'success',
+                title: '띵동',
+                text: '로그인 성공!'
+              });
+            localStorage.setItem('token', e.data.token);
+            this.props.history.push('/');
+          })
+          .catch(e => {
+            console.log(e);
+            Swal.fire({
+                type: 'error',
+                title: '로그인 실패'
+              });
+            localStorage.removeItem('token');
+          });
+      };
+
     render() {
         return (
             <div className="Login">
@@ -9,13 +53,19 @@ class Login extends Component {
                     <div className="Login-Form-IdPw">
                         <div className="Login-Form-IdPw-onlyLine1">
                             <div className="Login-Form-IdPw-onlyLine1-value">ID</div>
-                            <input type="text" className="Login-Form-IdPw-onlyLine1-box" placeholder="아이디"/>
+                            <input
+                              type="text" className="Login-Form-IdPw-onlyLine1-box" placeholder="아이디" name="id" onChange={this.handleChange} autoComplete="off"
+                              value={this.state.id} 
+                            />
                         </div>
                         <div className="Login-Form-IdPw-onlyLine2">
                             <div className="Login-Form-IdPw-onlyLine2-value">PASSWORD</div>
-                            <input type="password" className="Login-Form-IdPw-onlyLine2-box" placeholder="비밀번호"/>
+                            <input
+                              type="password" className="Login-Form-IdPw-onlyLine2-box" placeholder="비밀번호" name="pw" onChange={this.handleChange} autoComplete="off"
+                              value={this.state.pw}
+                            />
                         </div>
-                        <input className="Login-Form-IdPw-btn" type="button" value="LOGIN"/>
+                        <input className="Login-Form-IdPw-btn" type="button" value="LOGIN" onClick={this.handleLogin}/>
                     </div>
                 </div>
             </div>
