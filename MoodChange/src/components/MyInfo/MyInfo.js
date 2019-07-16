1ba83cdcf51e9fe66ecdbac68d37fd2a0862c8b9
boggil3665 <boggil3665@daum.net>
2019-07-16 23:23:50 +0900

    [ADD] MyInfo 밑작업

--- MoodChange/src/components/MyInfo/MyInfo.js
+++ MoodChange/src/components/MyInfo/MyInfo.js
@@ -5,8 +5,19 @@ export default class MyInfo extends Component {
   render() {
     return (
       <div className="MyInfo">
-        <div>
+        <div children="MyInfo-NavBar">
+          <div className="MyInfo-NavBar-Profile">
+
+          </div>
+          <div className="MyInfo-NavBar-Account">
           
+          </div>
+          <div className="MyInfo-NavBar-MyBoard">
+          
+          </div>
+        </div>
+        <div className="MyInfo-Main">
+        
         </div>
       </div>
     )
